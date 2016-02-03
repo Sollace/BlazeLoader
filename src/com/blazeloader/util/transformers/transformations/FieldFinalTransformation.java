0e9b243b8689a6ed042efc47a09d87e0bee8400f
Sollace <mwchris@mweb.co.za>
2016-02-03 12:25:53 +0200

    * Fixed Method access transformers not doing anything -_-

--- src/com/blazeloader/util/transformers/transformations/FieldFinalTransformation.java
+++ src/com/blazeloader/util/transformers/transformations/FieldFinalTransformation.java
@@ -2,29 +2,19 @@ package com.blazeloader.util.transformers.transformations;
 
 import com.blazeloader.bl.obf.AccessLevel;
 
-import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldNode;
 
 public class FieldFinalTransformation extends FieldTransformation {
-    public final boolean finalValue;
+    private final boolean finalValue;
 
     public FieldFinalTransformation(String targetClass, String fieldName, boolean setFinalTo) {
         super(targetClass, fieldName);
         finalValue = setFinalTo;
     }
-
-    @Override
-    public boolean apply(ClassNode cls) {
-        String dotName = getDotName(cls.name);
-        boolean didApply = false;
-        if (dotName.equals(targetClass)) {
-            for (FieldNode field : cls.fields) {
-                if (isGlobal || field.name.equals(fieldName)) {
-                    field.access = setAccess(field.access, AccessLevel.PUBLIC, true, finalValue);
-                    didApply = true;
-                }
-            }
-        }
-        return didApply;
-    }
+    
+	@Override
+	protected void transformField(FieldNode field) {
+    	//BLMain.LOGGER_MAIN.logDebug("FieldFinalTransformation:" + dotName + "." + field.name + "\r\n\tTarget: " + fieldName);
+        field.access = setAccess(field.access, AccessLevel.PUBLIC, true, finalValue);
+	}
 }
--- src/com/blazeloader/util/transformers/transformations/MethodTransformation.java
+++ src/com/blazeloader/util/transformers/transformations/MethodTransformation.java
@@ -1,5 +1,8 @@
 package com.blazeloader.util.transformers.transformations;
 
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+
 public abstract class MethodTransformation extends Transformation {
     public final String methodName;
 
@@ -7,5 +10,22 @@ public abstract class MethodTransformation extends Transformation {
         super(targetClass, "*".equals(methodName));
         this.methodName = methodName;
     }
-
+    
+    @Override
+    public final boolean apply(ClassNode cls) {
+        String dotName = getDotName(cls.name);
+        boolean didApply = false;
+        if (dotName.equals(targetClass)) {
+            for (MethodNode method : cls.methods) {
+                String mName = method.name.concat(" ").concat(method.desc).replace('/', '.');
+                if (isGlobal || mName.equals(methodName)) {
+                	transformMethod(method);
+                    didApply = true;
+                }
+            }
+        }
+        return didApply;
+    }
+    
+    protected abstract void transformMethod(MethodNode method);
 }
