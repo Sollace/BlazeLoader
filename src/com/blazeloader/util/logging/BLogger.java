9daef101feabfa70ba72701d2c313ada11117b29
Sollace <mwchris@mweb.co.za>
2016-02-28 15:46:31 +0200

    Cleaned up Logging and the LogManager

--- src/com/blazeloader/util/logging/BLogger.java
+++ src/com/blazeloader/util/logging/BLogger.java
@@ -1,38 +1,33 @@
 package com.blazeloader.util.logging;
 
 import com.blazeloader.bl.main.Settings;
+
 import net.acomputerdog.core.logger.CLogger;
 import net.acomputerdog.core.logger.LogLevel;
 
 public class BLogger extends CLogger {
-
     private boolean logToFile = true;
-
+    
+    BLogger(String name, boolean includeDate, boolean includeTime) {
+        super(name, includeDate, includeTime, LogLevel.DEBUG);
+    }
+    
     BLogger(String name, boolean includeDate, boolean includeTime, LogLevel minimumLevel) {
         super(name, includeDate, includeTime, minimumLevel);
     }
-
-    @Override
-    public void logRaw(String message) {
-        super.logRaw(message);
-        if (logToFile && Settings.logToFile) {
-            LogManager.addLogEntry(this, formatMessage(message));
+    
+    protected String formatMessage(LogLevel level, String message) {
+    	String result = super.formatMessage(level, message);
+    	if (logToFile && Settings.logToFile) {
+            LogManager.addLogEntry(this, result + "\r\n");
         }
+    	return result;
     }
-
-    @Override
-    public boolean log(LogLevel level, String message) {
-        boolean result = super.log(level, message);
-        if (result && logToFile && Settings.logToFile) {
-            LogManager.addLogEntry(this, formatMessage(message) + "\n");
-        }
-        return result;
-    }
-
+    
     public boolean canLogToFile() {
         return logToFile;
     }
-
+    
     public void setLogToFile(boolean logToFile) {
         this.logToFile = logToFile;
     }
