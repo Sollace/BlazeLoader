5925d80916e5b4409bd9d9abf2c13474ff929d71
Sollace <mwchris@mweb.co.za>
2016-03-22 12:14:32 +0200

    Bump version

--- src/main/com/blazeloader/util/version/Versions.java
+++ src/main/com/blazeloader/util/version/Versions.java
@@ -13,7 +13,7 @@ public class Versions {
     private static final boolean isOBF = VersionUtils.isGameOBF();
     private static final boolean isForgeInstalled = VersionUtils.hasForge();
     private static final Map<String, Version> versionMap = new HashMap<String, Version>();
-    private static final Version BL_VERSION = new BasicVersion("BlazeLoader.main", "BlazeLoader", BuildType.DEVELOPMENT, 1, 1, 1);
+    private static final Version BL_VERSION = new BasicVersion("BlazeLoader.main", "BlazeLoader", BuildType.BETA, 1);
 
     /**
      * Detects if the game is obfuscated.
